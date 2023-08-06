975810ef6c2d700210a943170eb1bc4962705786
DuyKhanh <caankhwh1303@gmail.com>
2023-08-06 19:36:12 +0700

    Add project files.

---
+++ MyAPINetCore6/Data/BookStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyAPINetCore6.Data
+{
+    public class BookStoreContext:DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> opt):base(opt) 
+        { 
+        }
+        #region DBSet
+        public DbSet<Book>? Books { get; set; }
+        #endregion
+    }
+}
---
+++ MyAPINetCore6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPINetCore6.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// để api của mình cho mọi người sài => addCors
+// lí do: default của trình duyệt mình chỉ cho phép kết nối với các resourse trong cùng cái web đó thôi
+// mà nếu API chỉ cho cùng API mình gọi thì đâu còn ý nghĩa của API
+// API mình định nghĩa ,pulblic , expose công bố ra các endpoint cho những ứng dụng khác sài
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+}));
+
+//đăng kí khai báo kết nối dbcontext
+builder.Services.AddDbContext<BookStoreContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoreString"));
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
